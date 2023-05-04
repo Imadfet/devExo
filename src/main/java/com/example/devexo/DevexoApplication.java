9c5f64ecd11b5ed3018ce49c565e18a77db15096
Imadfet <imadfettah7@gmail.com>
2023-05-04 22:09:06 +0100

    deuxiemee message

--- src/main/java/com/example/devexo/DevexoApplication.java
+++ src/main/java/com/example/devexo/DevexoApplication.java
@@ -1,13 +1,12 @@
 package com.example.devexo;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DevexoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("helloo");
+		System.out.println("heelloo");
 	}
 
 }
