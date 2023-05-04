d97f97f77f80d9a4f61ab9d1829037efeb14cf53
Imadfet <imadfettah7@gmail.com>
2023-05-04 21:47:45 +0100

    premier message

---
+++ src/main/java/com/example/devexo/DevexoApplication.java
@@ -0,0 +1,13 @@
+package com.example.devexo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevexoApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+	}
+
+}
---
+++ src/test/java/com/example/devexo/DevexoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devexo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevexoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
